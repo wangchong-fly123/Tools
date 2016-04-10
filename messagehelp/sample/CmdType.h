2b9c0a1162a27cc193181b696f2dc7162a17c707
wangchong <wangchong@iZ11gwfi6i5Z.(none)>
2016-04-10 15:48:38 +0800

    add message type

---
+++ messagehelp/sample/CmdType.h
@@ -0,0 +1,14 @@
+#ifndef _MESSAGE_CMDTYPE_H
+#define _MESSAGE_CMDTYPE_H
+#include "zType.h"
+///////////////////////////////////////////////
+//
+//code[CmdType.h] 由 messagehelp 生成,请勿修改
+//
+///////////////////////////////////////////////
+
+namespace Cmd
+{
+    const BYTE HERO_CARD_USERCMD = 162; //card command
+} //end of namespace Cmd
+#endif
